fc31436cc0911b7f381356688613ccee22561160
Samar Dhital <137791769+STAPARMY@users.noreply.github.com>
2023-11-02 16:41:54 +0545

    Update cam.sh

--- cam.sh
+++ cam.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-link =$http-server -p 3333
-if [[$options_tem -eq 1]];then 
-sed 's+forwarding_link+'$link'+g'live.html
+
+
+link=$(http-server -p 3333)
+
+# Check if the variable $options_tem is equal to 1
+if [[ $options_tem -eq 1 ]]; then
+    # Replace the placeholder "forwarding_link" with the generated link in the live.html file
+    sed 's+forwarding_link+'$link'+g' live.html > live_updated.html
+    mv live_updated.html live.html
+fi
